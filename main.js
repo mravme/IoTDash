cae32b13cae929afe389269cdb1a447353bc9e95
mravme <33031559+mravme@users.noreply.github.com>
2020-11-12 12:09:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let ctrl = require('./mBetGrbr');
+let data = require('./dataLoop');
+let client = require('./conn/connServer');
+async function main(){
+        loop=true;
+        do{
+        }while(loop);
+}
+main();
